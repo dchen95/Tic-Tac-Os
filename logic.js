7aa82811ed6b4b6f6e829d9b7893bd566e4b58c8
Kamran Grasselli <kfg09@yahoo.com>
2018-04-04 23:12:41 -0400

    Added some Firebase code - not working

--- logic.js
+++ logic.js
@@ -13,10 +13,8 @@ var database = firebase.database();
 var game = database.ref("/game_stats");
 
 // Branches to store each player's actions
-var player1 = database.ref("/player1");
-var player2 = database.ref("/player2");
-
-player1.set(true)
+var player1F = database.ref("/player1");
+var player2F = database.ref("/player2");
 
 // Variables for players' names
 var player1Name;
@@ -26,7 +24,7 @@ var player2Name;
 
 var winningCombos = []; // stores winning combinations
 var currentState = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]; // current state of game board
-var currentPlayer = 1; // tracks whose turn it is
+var currentPlayer = 1; // tracks whose turn it is -- set to zero when stopped
 var player1score = 0;
 var player2score = 0;
 var numberMoves = 0; // tracks total number clicks in game; calculations begin after 5 clicks
@@ -40,23 +38,27 @@ player2Name = $('#player2-name').text().trim();
 loadWinningCombos();  // load array of winning combos
 console.log("Loaded winningCombos: " + winningCombos);
 
+function loadWinningCombos() {
+    winningCombos.push([0, 1, 2]);
+    winningCombos.push([3, 4, 5]);
+    winningCombos.push([6, 7, 8]);
+    winningCombos.push([0, 3, 6]);
+    winningCombos.push([1, 4, 7]);
+    winningCombos.push([2, 5, 8]);
+    winningCombos.push([0, 4, 8]);
+    winningCombos.push([2, 4, 6]);
+};
 
 //==================================================
 
-game.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    
-});
-
 function playGame(clickedId) {
     console.log("clickedId = " + clickedId); // display number of box clicked
-    console.log("currentPlayer: " + currentPlayer);
+    console.log("currentPlayer: " + currentPlayer);    
 
     gameStop = 0;
     numberMoves++;
-  
-    
-    // mark "X" for Player 1
+      
+    // update board array (currentState)
     if (currentPlayer == 1) {
         
         currentState.splice((clickedId-1), 1, "X");  // save play in array position 'clickedId-1'
@@ -64,25 +66,25 @@ function playGame(clickedId) {
         
         // $('#' + clickedId).attr('src', 'assets/images/x.png'); // assign "X" image to img-src
         // flipSquare(clickedId);
-        
+
         currentPlayer = 2;
         console.log("currentPlayer switched to: " + currentPlayer);
         
+
     }
-    else {
-        // mark "O" for Player 2
-        
+    else {       
         currentState.splice((clickedId-1), 1, "O"); // save play in array position 'clickedId-1'
         console.log('currentState:  ' + currentState);
         
-        $('#' + clickedId).attr('src', 'assets/images/o.png'); // assign "O" image to img-src
-        flipSquare(clickedId);
+        // $('#' + clickedId).attr('src', 'assets/images/o.png'); // assign "O" image to img-src
+        // flipSquare(clickedId);
         
         currentPlayer = 1;
         console.log("currentPlayer switched to: " + currentPlayer);
         
     }
     
+    // write values to Firebase
     game.set({
         currentStateF: currentState,
         currentPlayerF: currentPlayer,
@@ -91,9 +93,10 @@ function playGame(clickedId) {
         numberMovesF: numberMoves,
         gameStopF: gameStop,
         clickedIdF: clickedId
-    })
+    });
+
     
-    
+    // determine if there's a winner
     if (numberMoves > 4) { // only start checking after 5 clicks
         for (var i = 0; i < winningCombos.length; i++) {
             // compare currentState array with all winningCombos nested arrays to test for match
@@ -114,7 +117,8 @@ function playGame(clickedId) {
                 $('#subtitle').text("Player 1 wins");
                 player1score++;  // add point to Player1
                 gameStop = 1;
-                numberMoves = 0;  // reset moves counter
+                currentPlayer = 0;
+                numberMoves = 0;  // resets moves counter
                 break;
             }
             else if 
@@ -125,40 +129,51 @@ function playGame(clickedId) {
                 $('#subtitle').text("Player 2 wins");
                 player2score++;  // add point to Player2
                 gameStop = 1;
-                numberMoves = 0; // reset moves counter
+                currentPlayer = 0;
+                numberMoves = 0; // resets moves counter
                 break;
             }
             
             else if ((numberMoves == 9) && (gameStop == 0)) {  // conditions for a tie
                 $('#subtitle').text("It's a tie!");
                 gameStop = 1;
-                numberMoves = 0; // reset moves counter
+                currentPlayer = 0;
+                numberMoves = 0; // resets moves counter
                 break;
             }
         }
     }
+    // detect changes in Firebase and update screen accordingly
+    game.on("value", function(snapshot) {
+        console.log("snapshot.val():  " + snapshot.val());
+        console.log("game.currentPlayerF:  " + game.currentPlayerF);
+        
+    
+        if (game.currentPlayerF == 1) {
+            $('#' + clickedId).attr('src', 'assets/images/x.png'); // assign "X" image to img-src
+            flipSquare(clickedIdF);
+        }
+        
+        if (game.currentPlayerF == 2) {
+            $('#' + clickedId).attr('src', 'assets/images/o.png'); // assign "O" image to img-src
+            flipSquare(clickedIdF);
+        }
+    });
+    
 }
 
-function loadWinningCombos() {
-    winningCombos.push([0, 1, 2]);
-    winningCombos.push([3, 4, 5]);
-    winningCombos.push([6, 7, 8]);
-    winningCombos.push([0, 3, 6]);
-    winningCombos.push([1, 4, 7]);
-    winningCombos.push([2, 5, 8]);
-    winningCombos.push([0, 4, 8]);
-    winningCombos.push([2, 4, 6]);
-};
 
+
+// flip the block to the player's mark
 function flipSquare(clickedId) {
     console.log("clickedId in flipSquare: " + clickedId);
     var currentSquareId = $('#square' + clickedId).attr('id'); // display id of square clicked
     console.log(currentSquareId);
     
-    $('#square' + clickedId).flip({
-        axis: "y",
-        trigger: "click"
-    })
+        $('#square' + clickedId).flip({
+            axis: "y",
+            trigger: "click"
+        })
 };
 
 
